1a548718f0ea9ed5a6ae27837960d22e3b7999a5
Matthias Gattermeier <matthias.gattermeier@nytimes.com>
2016-01-29 14:30:40 -0500

    adds react router`

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import Router from 'react-router';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import Router from 'react-router';
 import routes from './routes';
 
 ReactDOM.render(<Router routes={routes} />, document.getElementById('root'));
